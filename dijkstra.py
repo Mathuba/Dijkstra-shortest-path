b1a6d284105c0bf63dc2ef532f0637f219fdb4dc
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-19 13:05:57 +0200

    Added add_edge function to build a directed weighted graph

--- dijkstra.py
+++ dijkstra.py
@@ -4,22 +4,27 @@ import sys
 import queue
 
 
+VERTEX = 0
+WEIGHT = 1
+def add_edge(graph, vert1, vert2, weight):
+    weighted_edge = [vert2, weight]
+    if vert1 in graph:
+        graph[vert1].append(weighted_edge)
+    else:
+        graph[vert1] = weighted_edge
+
+
 def distance(adj, cost, s, t):
     #write your code here
     return -1
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    s, t = data[0] - 1, data[1] - 1
-    print(distance(adj, cost, s, t))
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+    for _ in range(m):
+        vert1, vert2, weight = map(int, sys.stdin.readline().split())
+        add_edge(graph, vert1, vert2, weight)
+
+    start, end = map(int, sys.stdin.readline().split())
+    print(distance(graph, cost, s, t))
