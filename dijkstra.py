754c72a5f81f44e52e1016f09b2fac82dde917d9
Mathuba <Mathuba58@gmail.com>
2019-06-25 21:54:02 +0200

    Completed function to calculate shortest path

--- dijkstra.py
+++ dijkstra.py
@@ -1,11 +1,15 @@
 #Uses python3
 
 import sys
-import queue
+import math
+import heapq
 
 
 VERTEX = 0
 WEIGHT = 1
+INFINITY = float("inf")
+
+
 def add_edge(graph, vert1, vert2, weight):
     weighted_edge = [vert2, weight]
     if vert1 in graph:
@@ -14,9 +18,29 @@ def add_edge(graph, vert1, vert2, weight):
         graph[vert1] = weighted_edge
 
 
-def distance(adj, cost, s, t):
-    #write your code here
-    return -1
+def distance(graph, start, destination):
+    dist = [[INFINITY, i] for i in range((len(graph)+1))]
+    prev = [None for i in range((len(graph)+1))]
+    dist[start][0] = 0
+    prev[start] = start
+    pq = []
+
+    heapq.heappush(pq, dist[start])
+    while pq:
+        u_dist, u_vert = heapq.heappop(pq)
+        if u_dist == dist[u_vert][0]:
+            for v in graph[u_vert]:
+                v_vert, w_uv = v
+                if dist[v_vert][0] > dist[u_vert][0] + w_uv:
+                    dist[v_vert][0] = dist[u_vert][0] + w_uv
+                    heapq.heappush(pq, dist[v_vert])
+                    prev[v_vert] = u_vert
+    min_weight = dist[destination][0]
+
+    if min_weight == INFINITY:
+        return -1
+    else:
+        return min_weight
 
 
 if __name__ == '__main__':
@@ -26,5 +50,5 @@ if __name__ == '__main__':
         vert1, vert2, weight = map(int, sys.stdin.readline().split())
         add_edge(graph, vert1, vert2, weight)
 
-    start, end = map(int, sys.stdin.readline().split())
-    print(distance(graph, cost, s, t))
+    start, destination = map(int, sys.stdin.readline().split())
+    print(distance(graph, start, destination))
